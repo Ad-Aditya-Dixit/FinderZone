833bb5739e614dd1e6bb1d0cf97ff8404255e7f9
Aditya-sketch-hue <121467666+Aditya-sketch-hue@users.noreply.github.com>
2025-11-07 11:23:06 +0530

    Working version with YOLO object detection

--- app.py
+++ app.py
@@ -159,5 +159,6 @@ def uploaded_file(filename):
 # ---------------- MAIN APP ENTRY ----------------
 if __name__ == '__main__':
     init_db()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 10000))  # default fallback
+    app.run(host='0.0.0.0', port=port)
+
